d2d8028609e4a8122a8ba2ea1e01e1ead136006a
koryab <pergirolman@google.com>
2023-11-01 13:44:07 +0300

    Task 2 done

--- task_2.py
+++ task_2.py
@@ -1,20 +1,25 @@
 from argparse import ArgumentParser
 
 def parse_args():
-    parser = ArgumentParser(description='Process some integers.')
-    parser.add_argument('points', type=int, nargs='+',
-                        help='coordinates of points A, B, C, D respectively.')
+    parser = ArgumentParser(description="Process some integers.")
+    parser.add_argument("-c", "--coords",
+                        nargs="+",
+                        required=True,
+                        action="store",
+                        help="coordinates of points A, B, C, D respectively.")
+    args = parser.parse_args()
 
-    return parser.parse_args()
+    return args
 
-def count_ratio(points):
-    a, b, c, d = points
+def count_ratio(coords):
+    a, b, c, d = list(map(float, coords))
 
     return round(abs((a - b)/(c - d)), 6) 
 
 def main():
     args = parse_args()
-    print(f"Legths ratio {count_ratio(args)}")
+    coords = args.coords
+    print(f"Legths ratio {count_ratio(coords):.6}")
 
 if __name__ == "__main__":
     main()
